8fc029847c725d1d575cc161f23ee10780be3d1c
Romain_Bdt <56961864+Romitchko@users.noreply.github.com>
2022-01-31 11:37:49 +0100

    maj test
    maj test

--- src/com/yoann/calcul/CalculsTest.java
+++ src/com/yoann/calcul/CalculsTest.java
@@ -1,27 +1,72 @@
-package com.yoann.calcul;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 
+ */
+
+/**
+ * @author test
+ * 
+ */
 class CalculsTest {
 
-	@BeforeEach
-	public void initTest()
+	private Calculs c1 = null;
+	private Calculs c2 = null;
+	private Calculs c3 = null;
+	
+	/**
+	 * Initialise les valeurs avant chaque test 
+	 */
+	@BeforeEach // D�clencher cette m�thode avant l'ex�cution
+	void setUp() throws Exception 
 	{
-		Calculs c = new Calculs(5,10);
+		c1 = new Calculs(1,2);
+		c2 = new Calculs(10,20);
+		c3 = new Calculs(100,200);
 	}
+
 	
-	
+
+	/**
+	 * Test method for {@link Calculs#multiplier()}.
+	 */
 	@Test
-	public void test() 
+	void testMultiplier() 
 	{
+		if(c1.multiplier() != 2)
+		{
+			fail("Methode multiplier non conforme 2*1=2 ne fonctionne pas.");
+		}
 		
+		// Est ce que (2 == 42) ? non donc : test en Erreur
+		assertEquals(c1.multiplier(), 2);
 		
+	}
+
+	/**
+	 * Test method for {@link Calculs#additionner()}.
+	 */
+	@Test
+	void testAdditionner() {
+	}
+
+	/**
+	 * Test method for {@link Calculs#diviser()}.
+	 */
+	@Test
+	void testDiviser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Calculs#soustraire()}.
+	 */
+	@Test
+	void testSoustraire() {
+		fail("Not yet implemented");
 	}
 
 }
